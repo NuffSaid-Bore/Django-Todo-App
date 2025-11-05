0b8000332d1421effa3953c4a8932e569d894509
Nuffsaid-Bore <borekamohelol@gmail.com>
2025-11-05 14:44:41 +0200

    stsic files for local development

--- todo_site/todo_site/settings.py
+++ todo_site/todo_site/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# (Optional) Include additional static folders for local development
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
